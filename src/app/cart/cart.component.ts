f6fbdaf1c8c04442fcff6ab54be72e897c50050c
Kim-JunTae <rardin@naver.com>
2020-12-21 21:49:48 +0900

    2020.12.21 폼으로 입력 받기

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms'
 
 @Component({
   selector: 'app-cart',
@@ -8,13 +9,27 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit{
   items;
+  checkoutForm;
 
   constructor(
-    private cartService: CartService
-  ) { }
+    private cartService: CartService,
+    private formBuiler: FormBuilder,
+  ) {
+    this.checkoutForm = this.formBuiler.group({
+      name: '',
+      address: ''
+    });
+   }
 
   ngOnInit() {
     this.items = this.cartService.getItems();
   }
 
+  onSubmit(customerData){
+    //주문 로직은 여기에 구현합니다.
+    this.items = this.cartService.ClearCart();
+    this.checkoutForm.reset();
+
+    console.warn('Your order has been submitted', customerData);
+  }
 }
